d372ce343e6a70cabedd5dfd912677d89e72435d
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-09-10 19:24:47 +1200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chains.summarize import load_summarize_chain
 import tempfile
 from langchain.docstore.document import Document
 
-st.title("Kaiako")
+st.title("Māori translation bot")
 
 def maoriTranslator(maori_word):
     chat = ChatOpenAI(
@@ -28,7 +28,11 @@ def maoriTranslator(maori_word):
     result = chain.run(maori_word=maori_word)
     return result
 
-maori_word = st.text_input("Enter the Maori word or phrase you want to learn")
+maori_word = st.text_input(" Disclaimer:
+
+This Maori translation bot aims to assist with translations, but it may not always provide entirely accurate results. Translation accuracy can vary based on context and language nuances. For important or precise translations, consult with a proficient Maori speaker or language expert. The bot's translations should be used as a helpful reference but not solely relied upon. Use it with discretion.
+
+Enter the Maori word or phrase you want to learn")
 response = ""
 
 if st.button("Submit"):
