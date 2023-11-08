83a89db0da324059f0c3171bc0a37a82e023107d
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-11-09 10:03:12 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
 import streamlit as st
+from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+from langchain.document_loaders import *
+from langchain.chains.summarize import load_summarize_chain
+import tempfile
+from langchain.docstore.document import Document
 
-st.title("Māori Translation Bot")
+st.title("Māori translation bot")
 
 def maoriTranslator(maori_word):
     chat = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        max_tokens=50,
+        model="gpt-3.5-turbo-16k",
+        temperature=0
+    )
+    system_template = """You are an assistant that provides translations and explanations for Maori words and phrases."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    human_template = """Please provide the translation or explanation for the Maori word or phrase: '{maori_word}'."""
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
     )
 
-    system_template = "You are an assistant that provides translations and explanations for Maori words and phrases."
-    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-
-    human_template = f"Please provide the translation or explanation for the Māori word or phrase: '{maori_word}'."
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-    result = chat.get_response(chat_prompt, max_tokens=50, maori_word=maori_word)
-    
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    result = chain.run(maori_word=maori_word)
     return result
 
-maori_word = st.text_input("Enter the Māori word or phrase you want to learn:")
+maori_word = st.text_input("""Disclaimer:
+
+This Māori translation bot aims to assist with translations, but it may not always provide entirely accurate results. Translation accuracy can vary based on context and language nuances. For important or precise translations, consult with a proficient Māori speaker or language expert. The bot's translations should be used as a helpful reference but not solely relied upon. Use it with discretion.
+
+
+Enter the Māori word or phrase you want to learn:""")
 response = ""
 
-if st.button("Translate"):
+if st.button("Submit"):
     if maori_word:
         response = maoriTranslator(maori_word)
 
